d07c902cae9a4d064b2a875d62cf6c0ebf833194
Joel Varghese Joy <joelvjoy94@gmail.com>
2026-04-19 20:36:07 +0530

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.kt
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if(root == null) return null
+        
+        invertTree(root.left)
+        invertTree(root.right)
+        
+        val temp = root.left
+        root.left = root.right
+        root.right = temp
+        return root
+    }
+}
