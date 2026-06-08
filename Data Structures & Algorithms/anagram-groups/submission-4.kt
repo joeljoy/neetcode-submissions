a0b87101f7c7db06f48effa991a0697757965ee7
Joel Varghese Joy <joelvjoy94@gmail.com>
2026-06-08 10:02:58 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val freqMap = mutableMapOf<String,List<String>>()
+        for(str in strs){
+            val freq = getFreq(str)
+            freqMap[freq] = freqMap.getOrDefault(freq, listOf()).plus(str)
+        }
+        val result = mutableListOf<List<String>>()
+        for((key, value) in freqMap.entries){
+            result.add(value)
+        }
+        return result
+    }
+
+    private fun getFreq(str:String):String{
+        val freq = IntArray(26)
+        for(c in str){
+            freq[c - 'a'] += 1
+        }
+        return freq.joinToString(",")
+    }
+}
+
+
