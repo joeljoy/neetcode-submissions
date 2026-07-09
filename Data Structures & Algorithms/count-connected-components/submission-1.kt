2bed6331e938b579aad9597684560efd5bbf7e12
Joel Varghese Joy <joelvjoy94@gmail.com>
2026-07-09 11:46:07 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun countComponents(n: Int, edges: Array<IntArray>): Int {
+        var components = n
+        val parent = IntArray(n){
+            it
+        }
+
+        fun find(x:Int):Int{
+            if(parent[x] == x) return x
+            return find(parent[x])
+        }
+
+        fun union(x:Int, y:Int){
+            val rootx = find(x)
+            val rooty = find(y)
+
+            if(rootx != rooty){
+                parent[rooty] = rootx
+                components -= 1
+            }
+        }
+
+       
+        for(edge in edges){
+            union(edge[0], edge[1])
+        }
+        return components
+    }
+}
