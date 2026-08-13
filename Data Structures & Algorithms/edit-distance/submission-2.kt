703e60c7f3381ca4d4dbd71d5a549f458b79439c
Joel Varghese Joy <joelvjoy94@gmail.com>
2026-08-13 10:51:31 +0530

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun minDistance(word1: String, word2: String): Int {
+        val m = word1.length
+        val n = word2.length
+
+        val dp = Array(m + 1){
+            IntArray(n + 1)
+        }
+        
+        for(i in 1..m){
+            dp[i][0] = i
+        }
+
+        for(j in 1..n){
+            dp[0][j] = j
+        }
+
+        for(i in 1..m){
+            for(j in 1..n){
+                dp[i][j] = if(word1[i - 1] == word2[j - 1]){
+                    dp[i-1][j-1]
+                }else{
+                    1 + minOf(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+                }
+            }
+        }
+        return dp[m][n]
+    }
+}
